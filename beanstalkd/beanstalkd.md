6bff9c6e603d56a2707fb737c866d31705e836b7
cw <chudaozhe@outlook.com>
2016-03-27 22:36:07 +0800

    retry(频率为15/15/30/180/1800/1800/1800/1800/3600，单位：秒)

--- beanstalkd/beanstalkd.md
+++ beanstalkd/beanstalkd.md
@@ -9,11 +9,11 @@ make && make install
 
 ##开启服务
 ##默认端口11300
-beanstalkd -l 192.168.9.102 -c &
+beanstalkd -l 192.168.9.102 -b /data/apps/beanstalkd/ &
 
 ##启动选项
 
--b DIR   wal directory
+-b DIR   wal directory[开启binlog，断电后重启会自动恢复任务。]
 -f MS    fsync at most once every MS milliseconds (use -f0 for “always fsync”)
 -F       never fsync (default)
 -l ADDR  listen on address (default is 0.0.0.0)
@@ -35,6 +35,8 @@ https://github.com/jimbojsb/bstools/archive/v0.5.tar.gz
 cd bin/
 php bs.php stats --host="192.168.9.102"
 
+#Chrome 插件
+https://chrome.google.com/webstore/detail/beanstalkd-dashboard/dakkekjnlffnecpmdiamebeooimjnipm
 
 #php客户端
 https://github.com/davidpersson/beanstalk
@@ -43,6 +45,4 @@ https://github.com/davidpersson/beanstalk/archive/v1.0.0.tar.gz
 #参考：
 http://blog.chedushi.com/archives/8026
 http://blog.csdn.net/black_ox/article/details/24792489
-
-#下一步要实现 失败定期重试（通知频率为15/15/30/180/1800/1800/1800/1800/3600，单位：秒）
-
+https://segmentfault.com/a/1190000002784775
--- beanstalkd/demo/result.php
+++ beanstalkd/demo/result.php
@@ -17,7 +17,6 @@ if (!$beanstalk->connect()) {
 // var_dump($beanstalk->stats());
 //查看有多少个tube
 // var_dump($beanstalk->listTubes());
-$beanstalk->useTube('test');
 //设置要监听的tube
 $beanstalk->watch('test');
 //取消对默认tube的监听，可以省略
@@ -26,25 +25,53 @@ $beanstalk->ignore('default');
 // var_dump($beanstalk->listTubesWatched());
 //查看test的tube当前的状态
 // var_dump($beanstalk->statsTube('test'));
+/**
+ * //延迟写入
+ * put->delayed->ready
+ * //立即写入
+ * put->ready->reserved(锁定，其他用户不能再操作)
+ * reserved->delete(delete之后，job从系统消亡，之后不能再获取)
+ * reserved->release(释放，release操作可以重新把该job状态迁移回READY（也可以延迟该状态迁移操作），使其他的consumer可以继续获取和执行该job)
+ * reserved->bury(保留，把该job休眠，等到需要的时候，再将休眠的job kick回READY状态，也可以delete BURIED状态的job)->buried
+ * 
+ * READY - 需要立即处理的任务，当延时 (DELAYED) 任务到期后会自动成为当前任务；
+ * DELAYED - 延迟执行的任务, 当消费者处理任务后, 可以用将消息再次放回 DELAYED 队列延迟执行；
+ * RESERVED - 已经被消费者获取, 正在执行的任务。Beanstalkd 负责检查任务是否在 TTR(time-to-run) 内完成；
+ * BURIED - 保留的任务: 任务不会被执行，也不会消失，除非有人把它 "踢" 回队列；
+ * DELETED - 消息被彻底删除。Beanstalkd 不再维持这些消息。
+ */
 while (true) {
     //获取任务，此为阻塞获取，直到获取有用的任务为止
-    $job = $beanstalk->reserve(); //返回格式array('id' => 123, 'body' => 'hello, beanstalk')
-        file_put_contents('hehe', json_encode($job)."\n\n\n", FILE_APPEND);
+    //返回格式array('id' => 123, 'body' => 'hello, beanstalk')
+    $job = $beanstalk->reserve(); 
     //处理任务
     $result = doJob($job['body']);
     if ($result) {
         //删除任务
         $beanstalk->delete($job['id']);
     } else {
-        //保留任务
-        $beanstalk->bury($job['id']);
+        $stats = $beanstalk -> statsJob($job['id']);
+        // file_put_contents('hehe', 'stats:'.date("Y-m-d H:i:s").':'.json_encode($stats)."\n", FILE_APPEND);
+
+        if ($stats['releases'] >=0 && $stats['releases'] <=8) {
+            //9次以下延时返回队列
+            $timer = array(15,15,30,180,1800,1800,1800,1800,3600);
+            // $timer = array(2,3,3,4,5,5,5,6,6);
+            $beanstalk->release($job['id'], 0, $timer[$stats['releases']]);
+            
+        }else{
+            //错误次数过多时 bury
+            $beanstalk->bury($job['id']);
+        }
+
     }
     
 }
-$beanstalk->disconnect();
+// $beanstalk->disconnect();
 
 function doJob($job)
 {
+    return false;
     $data = json_decode($job, true);
     if (!is_array($data)) exit;
 
